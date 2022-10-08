f92dd6655ce192593124695adf5a6be8d548466e
Luan Daros <darosluan@gmail.com>
2022-10-08 19:13:26 -0300

    refactor JPA

--- src/test/java/br/edu/ifsul/cc/lpoo/cv/test/TestPersistenciaJPA.java
+++ src/test/java/br/edu/ifsul/cc/lpoo/cv/test/TestPersistenciaJPA.java
@@ -1,150 +1,120 @@
 
 package br.edu.ifsul.cc.lpoo.cv.test;
 
-import br.edu.ifsul.cc.lpoo.cv.model.Fornecedor;
-import br.edu.ifsul.cc.lpoo.cv.model.Produto;
-import br.edu.ifsul.cc.lpoo.cv.model.Receita;
-import br.edu.ifsul.cc.lpoo.cv.model.TipoProduto;
 import br.edu.ifsul.cc.lpoo.cv.model.dao.PersistenciaJPA;
+import br.edu.ifsul.cc.lpoo.cv.model.*;
+
 import java.util.Calendar;
 import java.util.List;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TestPersistenciaJPA {
 
     @Test
     public void testConexaoJPA() {
-        // criar um objeto do tipo PersistenciaJPA.
-        PersistenciaJPA jpa = new PersistenciaJPA();
-        if (jpa.conexaoAberta()) {
-            System.out.println("conectou no BD via jpa ...");
-            jpa.fecharConexao();
-        } else {
-            System.out.println("nao conectou no BD ...");
-
-        }
+        PersistenciaJPA JPA = new PersistenciaJPA();
+        assertTrue(JPA.conexaoAberta());
     }
 
     @Test
     public void testListProduto() throws Exception {
-        // criar um objeto do tipo PersistenciaJPA.
-        PersistenciaJPA jpa = new PersistenciaJPA();
+        PersistenciaJPA JPA = new PersistenciaJPA();
 
-        List<Produto> lista = jpa.listProdutos();
+        List<Produto> produtos = JPA.listProdutos();
 
-        if (lista != null) {
-            System.out.println("testListProduto : " + lista.size());
-
-            for (Produto p : lista) {
-                System.out.println("Produto: " + p);
-            }
-
+        for (Produto produto : produtos) {
+            System.out.println(produto);
         }
 
-        jpa.fecharConexao();
+        JPA.fecharConexao();
     }
 
     @Test
     public void testPersitenciaProduto() throws Exception {
-        // criar um objeto do tipo PersistenciaJPA.
-        PersistenciaJPA jpa = new PersistenciaJPA();
-        if (jpa.conexaoAberta()) {
+        PersistenciaJPA JPA = new PersistenciaJPA();
+        List<Produto> produtos = JPA.listProdutos();
 
-            List<Produto> lista = jpa.listProdutos();
-            if (lista.isEmpty()) {
+        if (produtos.isEmpty()) {
+            Produto novoProduto = new Produto();
+            novoProduto.setNome("teste");
+            novoProduto.setFornecedor(getFornecedor(JPA));
+            novoProduto.setQuantidade(0f);
+            novoProduto.setTipo(TipoProduto.ATENDIMENTO_AMBULATORIAL);
+            novoProduto.setValor(100f);
 
-                Produto p = new Produto();
-                p.setNome("teste");
-                p.setFornecedor(getFornecedor(jpa));
-                p.setQuantidade(0f);
-                p.setTipo(TipoProduto.ATENDIMENTO_AMBULATORIAL);
-                p.setValor(100f);
-                jpa.persist(p);
-                System.out.println("Incluiu o produto: " + p.getId());
-
-            } else {
-
-                for (Produto p : lista) {
-
-                    p.setNome(p.getNome() + " - nome alterado");
-                    jpa.persist(p);// altera o registro a partir do objeto gerenciado (p).
-
-                    jpa.remover(p);// remove o registro a partir do objeto gerenciado (p).
-                }
-
-                System.out.println("Removeu todos os " + lista.size() + " produtos");
-            }
-
-            jpa.fecharConexao();
+            JPA.persist(novoProduto);
+            System.out.println("Incluiu o produto: " + novoProduto.getId());
         } else {
-            System.out.println("nao conectou no BD ...");
+            for (Produto produto : produtos) {
+                produto.setNome(produto.getNome() + " - nome alterado");
 
+                // altera o registro a partir do objeto gerenciado (p).
+                JPA.persist(produto);
+                // remove o registro a partir do objeto gerenciado (p).
+                JPA.remover(produto);
+            }
         }
+
+        System.out.println("Removeu todos os " + produtos.size() + " produtos");
+        JPA.fecharConexao();
     }
 
     @Test
     public void testPersitenciaReceita() throws Exception {
-        // criar um objeto do tipo PersistenciaJPA.
-        PersistenciaJPA jpa = new PersistenciaJPA();
-        if (jpa.conexaoAberta()) {
+        PersistenciaJPA JPA = new PersistenciaJPA();
 
-            List<Receita> lista = jpa.listReceitas();
-            if (lista.isEmpty()) {
+        List<Receita> receitas = JPA.listReceitas();
 
-                // criar uma objeto do tipo Receita
-                // vincular dois Produtos na receita.
+        if (receitas.isEmpty()) {
+            // criar uma objeto do tipo Receita
+            // vincular dois Produtos na receita.
 
-            } else {
+        } else {
 
-                for (Receita r : lista) {
+            for (Receita r : receitas) {
 
-                    // listar os produtos da receita
-                    // remover os produtos da receita
-                    // remover a receita.
-                    jpa.remover(r);
-                }
-
-                System.out.println("Removeu todas as " + lista.size() + " receitas");
+                // listar os produtos da receita
+                // remover os produtos da receita
+                // remover a receita.
+                JPA.remover(r);
             }
 
-            jpa.fecharConexao();
-        } else {
-            System.out.println("nao conectou no BD ...");
-
+            System.out.println("Removeu todas as " + receitas.size() + " receitas");
         }
+
+        JPA.fecharConexao();
     }
 
-    private Fornecedor getFornecedor(PersistenciaJPA jpa) throws Exception {
-        // criar um objeto do tipo PersistenciaJPA.
-        if (jpa.conexaoAberta()) {
+    // retorna um objeto do tipo Fornecedor gerenciado pelo JPA
+    private Fornecedor getFornecedor(PersistenciaJPA JPA) throws Exception {
 
-            List<Fornecedor> lista = jpa.listFornecedores();
-            if (lista.isEmpty()) {
+        if (JPA.conexaoAberta()) {
+            List<Fornecedor> fornecedores = JPA.listFornecedores();
 
-                Fornecedor f = new Fornecedor();
-                f.setNome("teste");
-                f.setCnpj("08316535000");
-                f.setIe("");
-                f.setData_cadastro(Calendar.getInstance());
-                f.setNome("Laboratório Santa Inês");
-                f.setRg("123");
-                f.setSenha("123");
-                f.setCpf("00001337788");
-                jpa.persist(f);
+            if (fornecedores.isEmpty()) {
 
-                return f;
+                Fornecedor fornecedorNovo = new Fornecedor();
+                fornecedorNovo.setNome("teste");
+                fornecedorNovo.setCnpj("08316535000");
+                fornecedorNovo.setIe("");
+                fornecedorNovo.setData_cadastro(Calendar.getInstance());
+                fornecedorNovo.setNome("Laboratório Santa Inês");
+                fornecedorNovo.setRg("123");
+                fornecedorNovo.setSenha("123");
+                fornecedorNovo.setCpf("00001337788");
 
+                JPA.persist(fornecedorNovo);
+
+                return fornecedorNovo;
             } else {
-
-                return lista.get(0);
+                return fornecedores.get(0);
             }
-
-        } else {
-            System.out.println("nao conectou no BD ...");
 
         }
 
-        return null;
+        throw new Exception("Não foi possível conectar no banco de dados");
     }
+
 }
